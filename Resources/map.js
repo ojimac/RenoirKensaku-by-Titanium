fd1d94005afd4cc5c468e36a997dae5b78ac0093
ojimac <ojimac@gmail.com>
2012-05-19 17:44:00 +0900

    sqliteを使ってピンを立てるところまで

--- Resources/map.js
+++ Resources/map.js
@@ -33,15 +33,30 @@ win.addEventListener('open', function() {
       userLocation: true,
     });
 
-    // 位置情報を使う事をアラートで示す
-    mapview.addAnnotation(Ti.Map.createAnnotation({
-      animate: true,
-      pincolor: Ti.Map.ANNOTATION_RED,
-      title: '現在位置',
-      latitude: latitude,
-      longitude: longitude,
-      leftButton: '/images/delete.png'
-    }));
+    // TODO: 近くにあるルノアールを検索して、ピンを立てる
+    var db = Titanium.Database.open('mydb');
+    var results = [];
+    db.execute('SELECT * FROM shops');
+    var rows = db.execute('SELECT * FROM shops');
+    while (rows.isValidRow()) {
+      // rows.field(field_no)で値を取得します。
+      // カラム名ベースでも取れるみたいです。
+      Ti.API.info('ID: ' + rows.field(0) + ' NAME: ' + rows.fieldByName('name'));
+      results.push(Ti.Map.createAnnotation({
+        animate: true,
+        pincolor: Ti.Map.ANNOTATION_RED,
+        title: '現在位置',
+        latitude: rows.fieldByName('lat'),
+        longitude: rows.fieldByName('lon'),
+        leftButton: '/images/delete.png'
+      }));
+      rows.next();
+    }
+    rows.close();
+    db.close();
+
+    // ピンを立てる(複数)
+    mapview.addAnnotations(results);
 
     // アラートを出したときに、左側の「設定」が押されたかどうか
     mapview.addEventListener('click', function(e) {
@@ -55,15 +70,6 @@ win.addEventListener('open', function() {
       }
     });
 
-    // ピンを立てる
-    var currentPos = Ti.Map.createAnnotation({
-      latitude: latitude,
-      longitude: longitude,
-      title:"現在地",
-      pincolor:Ti.Map.ANNOTATION_RED,
-      animate:true
-    });
-    mapview.addAnnotation(currentPos);
     // 隠していた地図を表示する
     mapview.show();
     // 現在地まで地図をスクロールする
@@ -75,11 +81,12 @@ win.addEventListener('open', function() {
     });
 
     /*
+    // TODO: 近くにあるルノアールを検索して、ピンを立てる
     mapview.addEventListener('regionChanged', function(e) {
       // 表示領域が変わったときのイベント（移動・拡大縮小）
       Ti.API.info('mapview redionChanged...');
       Ti.API.info('the new latitude = ' + e.latitude);
-      Ti.API.info('the new latitude delta = ' + e.latitudeDelta);    
+      Ti.API.info('the new latitude delta = ' + e.latitudeDelta);
       Ti.API.info('the new longitude = ' + e.longitude);
       Ti.API.info('the new longitude delta = ' + e.longitudeDelta);
     });
