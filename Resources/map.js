1f7e00fa19984e5ef7650dfb7ea7b602119c6a2a
ojimac <ojimac@gmail.com>
2012-05-24 22:35:58 +0900

    - ローディング画像追加 - データ取得部分、現在位置取得部分を関数化

--- Resources/map.js
+++ Resources/map.js
@@ -1,11 +1,30 @@
-//Ti.include('geo.js');
-
-var geo, mapview, latitude, longitude;
+var mapview, activityIndicator;
+var latitude  = 35.659371;
+var longitude = 139.701033;
 
 win = Ti.UI.currentWindow;
 
+// ローディング画像
+activityIndicator = Ti.UI.createActivityIndicator({
+  color: '',
+  font: {fontFamily:'Helvetica Neue', fontSize:26, fontWeight:'bold'},
+  message: '読込中...',
+  style:Ti.UI.iPhone.ActivityIndicatorStyle.DARK,
+  top:150,
+  left:100,
+  height:'auto',
+  width:'auto'
+});
+win.add(activityIndicator);
+
 // ウィンドウオープン
 win.addEventListener('open', function() {
+
+  activityIndicator.show();
+  setTimeout(function(e) {
+    e.source.close();
+    activityIndicator.hide();
+  }, 6000);
 
   // Make sure we only add the map once
   if (mapview !== undefined) {
@@ -13,50 +32,34 @@ win.addEventListener('open', function() {
   }
 
   // 現在位置を取得
+  get_location();
+
+  // map ビュー
+  mapview = Ti.Map.createView({
+    mapType: Ti.Map.STANDARD_TYPE,
+    region: {
+      latitude: latitude,
+      longitude: longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.10
+    },
+    animate: true,
+    regionFit: true,
+    userLocation: true,
+  });
+
   Ti.Geolocation.getCurrentPosition(function(e) {
 
     // 緯度・経度
     var longitude = e.coords.longitude;
     var latitude = e.coords.latitude;
 
-    // map ビュー
-    mapview = Ti.Map.createView({
-      mapType: Ti.Map.STANDARD_TYPE,
-      region: {
-        latitude: latitude,
-        longitude: longitude,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.10
-      },
-      animate: true,
-      regionFit: true,
-      userLocation: true,
-    });
 
-    // TODO: 近くにあるルノアールを検索して、ピンを立てる
-    var db = Titanium.Database.open('mydb');
-    var results = [];
-    db.execute('SELECT * FROM shops');
-    var rows = db.execute('SELECT * FROM shops');
-    while (rows.isValidRow()) {
-      // rows.field(field_no)で値を取得します。
-      // カラム名ベースでも取れるみたいです。
-      Ti.API.info('ID: ' + rows.field(0) + ' NAME: ' + rows.fieldByName('name'));
-      results.push(Ti.Map.createAnnotation({
-        animate: true,
-        pincolor: Ti.Map.ANNOTATION_RED,
-        title: '現在位置',
-        latitude: rows.fieldByName('lat'),
-        longitude: rows.fieldByName('lon'),
-        leftButton: '/images/delete.png'
-      }));
-      rows.next();
-    }
-    rows.close();
-    db.close();
+    // ルノアールの位置取得
+    annotations = get_all_renoir();
 
     // ピンを立てる(複数)
-    mapview.addAnnotations(results);
+    mapview.addAnnotations(annotations);
 
     // アラートを出したときに、左側の「設定」が押されたかどうか
     mapview.addEventListener('click', function(e) {
@@ -92,5 +95,59 @@ win.addEventListener('open', function() {
     });
     */
     win.add(mapview);
+
+    // 現在地を取得するボタン
+    button = Titanium.UI.createButton({
+     title: '現在地',
+     top: 10,
+     width: 100,
+     height:25
+    });
+
+    button.addEventListener('click', function() {
+      get_location();
+    });
+    win.add(button);
   });
 });
+
+// 現在位置を取得してスクロール
+var get_location = function() {
+  Ti.Geolocation.getCurrentPosition(function(e) {
+    // 緯度・経度
+    var longitude = e.coords.longitude;
+    var latitude = e.coords.latitude;
+
+    // 隠していた地図を表示する
+    mapview.show();
+    // 現在地まで地図をスクロールする
+    mapview.setLocation({
+      latitude:latitude,
+      longitude:longitude,
+      latitudeDelta:0.01,
+      longitudeDelta:0.01
+    });
+  });
+};
+
+// ルノアールを検索して、ピンを立てる
+var get_all_renoir = function() {
+  var db = Titanium.Database.open('mydb');
+  var results = [];
+  db.execute('SELECT * FROM shops');
+  var rows = db.execute('SELECT * FROM shops');
+  while (rows.isValidRow()) {
+    results.push(Ti.Map.createAnnotation({
+      animate: true,
+      pincolor: Ti.Map.ANNOTATION_RED,
+      title: rows.fieldByName('name'),
+      latitude: rows.fieldByName('lat'),
+      longitude: rows.fieldByName('lng'),
+      leftButton: '/images/delete.png'
+    }));
+    rows.next();
+  }
+  rows.close();
+  db.close();
+  return results;
+}
